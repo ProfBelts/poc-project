bb147fe11be5c6dfe74162cac45a3dd91badb9fe
Joseph Beltran <129695021+ProfBelts@users.noreply.github.com>
2024-09-23 22:08:47 +0800

    SaveStakeHolderInfoToDb

--- poc_project_Double Materiality Assessment/Controllers/HomeController.cs
+++ poc_project_Double Materiality Assessment/Controllers/HomeController.cs
@@ -47,34 +47,51 @@ namespace poc_project_Double_Materiality_Assessment.Controllers
 
         public IActionResult QuestionnaireSubmit(StakeholderInfo stakeholderInfo, Dictionary<int, int> issueRelevance, string additionalIssues)
         {
-            // Store the stakeholder info in TempData
-            TempData["StakeholderName"] = stakeholderInfo.StakeholderName;
-            TempData["Organization"] = stakeholderInfo.Organization;
-            TempData["Role"] = stakeholderInfo.Role;
-            TempData["Category"] = stakeholderInfo.Category;
-            TempData["AdditionalIssues"] = additionalIssues;
 
-            // Store issue relevance scores in TempData
-            TempData["IssueRelevance"] = Newtonsoft.Json.JsonConvert.SerializeObject(issueRelevance);
+            // Store the stakeholder info in TempData
+            //TempData["StakeholderName"] = stakeholderInfo.StakeholderName;
+            //TempData["Organization"] = stakeholderInfo.Organization;
+            //TempData["Role"] = stakeholderInfo.Role;
+            //TempData["Category"] = stakeholderInfo.Category;
+            //TempData["AdditionalIssues"] = additionalIssues;
+
+            //// Store issue relevance scores in TempData
+            //TempData["IssueRelevance"] = Newtonsoft.Json.JsonConvert.SerializeObject(issueRelevance);
+
+
+            var stakeholder = new Stakeholder()
+            {
+                Name = stakeholderInfo.StakeholderName,
+                Organization = stakeholderInfo.Organization,
+                Role = stakeholderInfo.Role,
+                Category = stakeholderInfo.Category
+            };
+
+            dbContext.Stakeholders.Add(stakeholder);
+
+            dbContext.SaveChanges();
+
 
             return RedirectToAction("ThankYou");
+
+
         }
 
 
         public IActionResult ThankYou()
         {
             // Deserialize issue relevance from TempData
-            var issueRelevanceJson = TempData["IssueRelevance"] as string;
-            var issueRelevance = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int, int>>(issueRelevanceJson);
+            //var issueRelevanceJson = TempData["IssueRelevance"] as string;
+            //var issueRelevance = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int, int>>(issueRelevanceJson);
 
-            // You can also access other TempData values here
-            ViewBag.StakeholderName = TempData["StakeholderName"];
-            ViewBag.Organization = TempData["Organization"];
-            ViewBag.Role = TempData["Role"];
-            ViewBag.Category = TempData["Category"];
-            ViewBag.AdditionalIssues = TempData["AdditionalIssues"];
+            //// You can also access other TempData values here
+            //ViewBag.StakeholderName = TempData["StakeholderName"];
+            //ViewBag.Organization = TempData["Organization"];
+            //ViewBag.Role = TempData["Role"];
+            //ViewBag.Category = TempData["Category"];
+            //ViewBag.AdditionalIssues = TempData["AdditionalIssues"];
 
-            return View(issueRelevance);
+            return View();
         }
 
 
